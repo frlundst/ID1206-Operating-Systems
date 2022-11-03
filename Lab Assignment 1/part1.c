9f0352d4489175183d54421ce8a43a53890b2711
Fredrik <fredrik.thomas.lundstrom@gmail.com>
2022-11-03 17:22:26 +0100

    Task 1 part 1 done.

--- Lab Assignment 1/part1.c
+++ Lab Assignment 1/part1.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char* argv[]){
-    printf("Hello World\n");
+int main(int argc, char *argv[])
+{
+    int fd[2];
+    pipe(fd);
+
+    switch (fork())
+    {
+    case -1: // fork error
+        exit(1);
+        break;
+
+    case 0: // child
+        dup2(fd[1], 1);
+        close(fd[0]);
+        execlp("ls", "ls", "/", NULL);
+        break;
+
+    default: // parent
+        wait(NULL);
+        dup2(fd[0], 0);
+        close(fd[1]);
+        execlp("wc", "wc", "-l", NULL);
+        break;
+    }
+
     return 0;
 }
