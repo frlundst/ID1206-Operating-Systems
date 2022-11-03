9969c094125e69f760f64ad091bd2f99f7b92781
Philip Hägg <philip.hagg@me.com>
2022-11-03 17:22:42 +0100

    annan lösning lol

--- Lab Assignment 1/part1.c
+++ Lab Assignment 1/part1.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h> 
+#include <fcntl.h> 
+#include <errno.h> 
+#include <sys/wait.h>
 
-int main(int argc, char* argv[]){
-    printf("Hello World\n");
-    return 0;
+int main(){
+        int pipefds[2];
+        int returnstatus;
+        int pid;
+
+        returnstatus = pipe(pipefds);
+        if (returnstatus == -1) {
+                printf("Unable to create pipe\n");
+                return 1;
+        }
+
+        pid = fork();
+
+        if (pid < 0) perror("Something failed on trying to create a child process!\n");
+        else if (pid == 0) {
+                dup2(pipefds[1], 1);
+                close(pipefds[0]);
+                execlp("ls", "ls", "/", NULL);
+                
+        } else {
+                dup2(pipefds[0], 0);
+                close(pipefds[1]);
+                execlp("wc", "wc", "-l", NULL);
+        }
+        return 0;
 }
