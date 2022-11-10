eee9828604759bd28b40dfcd68f80db95b4eb460
Philiphagg <62904448+Digitusmedia@users.noreply.github.com>
2022-11-10 13:52:22 +0100

    hello im under de watr

--- Lab Assignment 1/part2.c
+++ Lab Assignment 1/part2.c
@@ -1,45 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mqueue.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int main(int argc, char* argv[]){
     
 
 
-    switch (fork())
-    {
-    case -1: // fork error
-        exit(1);
-        break;
+    pid_t process = fork();
+    char* buffer = "Hello World";
 
-    case 0: // child
+    if (0 == process){
+
         FILE* file;
         char character;
-
         file = fopen("file.txt", "r");
 
         while((character = fgetc(file)) != EOF){
             printf("%c", character);
         }
 
-        mqd_t mqd = mq_open ("my_queue", O_CREAT | O_EXCL | O_WRONLY,  0600, NULL);
+        mqd_t mqd = mq_open("my_queue", O_CREAT | O_EXCL | O_WRONLY,  0600, NULL);
 
         if (mqd == -1) {
             perror ("mq_open");
             exit (1);
         }
-        mq_send (mqd, "Hello World", 11, 0);
+        mq_send (mqd,buffer , 11, 0);
         mq_close (mqd);
 
-
-        break;
-
-    default: // parent
-        mqd_t mqd = mq_open ("my_queue", O_RDONLY);
+    }else if(-1 == process){
+        exit(1);
+    }else{
+        mqd_t mqd = mq_open("my_queue", O_RDONLY);
         mq_receive (mqd, buffer, 8192, NULL);
         printf ("%s", buffer);
-
-        break;
     }
 
     return 0;
