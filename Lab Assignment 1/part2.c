9edb54a1ac6a67eefaf254fe3a8c17fd0a2548bc
Fredrik <fredrik.thomas.lundstrom@gmail.com>
2022-11-10 14:54:33 +0100

    Update part2.c

--- Lab Assignment 1/part2.c
+++ Lab Assignment 1/part2.c
@@ -5,14 +5,12 @@
 #include <sys/wait.h>
 #include <string.h>
 
-
-
 int main(int argc, char *argv[])
 {
     int msgid = msgget(IPC_PRIVATE, IPC_CREAT | 0600);
 
     if (msgid == -1)
-    { 
+    {
         perror("message queue error \n");
         exit(1);
     }
@@ -39,24 +37,31 @@ int main(int argc, char *argv[])
         int i;
         do
         {
-            message[i++] = fgetc(file);
+            ch = fgetc(file);
+            message[i++] = ch;
         } while (ch != EOF);
-        
-        printf("morsning");
+
         // Send file through message queue
         msgsnd(msgid, &message, 500, 0);
-        printf(" korsning");
     }
     else
     { // parent
-        wait(NULL);
+        (void)waitpid(id, NULL, 0);
         char rcv_message[500];
         msgrcv(msgid, &rcv_message, 500, 0, 0);
-        printf("punkt");
 
+        printf("%s \n", rcv_message);
+        
+        int count = 1;
+        for(int i = 0; rcv_message[i] != NULL; i++){
+            if(rcv_message[i] == ' ' || rcv_message[i] == '\n'){
+                count++;
+            }
+        }
+
+        printf("Number of words: %d \n", count);
         msgctl(msgid, IPC_RMID, NULL);
         exit(1);
-
     }
 
     return 0;
