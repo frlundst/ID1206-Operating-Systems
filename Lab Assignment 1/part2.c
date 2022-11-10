7ec41e4b3906df9e06fe73308df9f4c341b67b04
Philip Hägg <philip.hagg@me.com>
2022-11-10 14:38:44 +0100

    some changes

--- Lab Assignment 1/part2.c
+++ Lab Assignment 1/part2.c
@@ -5,11 +5,11 @@
 #include <sys/wait.h>
 #include <string.h>
 
+
+
 int main(int argc, char *argv[])
 {
-    key_t key;
-    int msgflg = IPC_CREAT | 0660;
-    int msgid = msgget(key, msgflg);
+    int msgid = msgget(IPC_PRIVATE, IPC_CREAT | 0600);
 
     if (msgid == -1)
     { 
@@ -29,25 +29,34 @@ int main(int argc, char *argv[])
     { // child
         FILE *file = fopen("part2.txt", "r");
 
-        if (file == NULL)
+        if (NULL == file)
         {
             perror("file can't be opened \n");
         }
 
         // Testar printa ut fil, lägg till i parent istället
-        /*char ch;
+        char message[500], ch;
+        int i;
         do
         {
-            ch = fgetc(file);
-            printf("%c", ch);
-        } while (ch != EOF);*/
-
+            message[i++] = fgetc(file);
+        } while (ch != EOF);
+        
+        printf("morsning");
         // Send file through message queue
-        msgsnd(key, &file, 500, msgflg);
+        msgsnd(msgid, &message, 500, 0);
+        printf(" korsning");
     }
     else
     { // parent
         wait(NULL);
+        char rcv_message[500];
+        msgrcv(msgid, &rcv_message, 500, 0, 0);
+        printf("punkt");
+
+        msgctl(msgid, IPC_RMID, NULL);
+        exit(1);
+
     }
 
     return 0;
