d0a8a873c748291f396a0fa52e1baec0bf19190a
Fredrik <fredrik.thomas.lundstrom@gmail.com>
2022-11-16 17:07:33 +0100

    Update lab2.c

--- Lab Assignment 2/lab2.c
+++ Lab Assignment 2/lab2.c
@@ -1,39 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>  
+#include <unistd.h>
 #include <pthread.h>
- 
+
 int buffer = 0;
 pthread_mutex_t lock;
 
-void *thread_function(void *vargp)
+int *thread_function(void *vargp)
 {
-    
-    int *myid = (int *)vargp;
-    
+    int *myid = (int *)vargp, iterator = 0;
+
+    while (1)
+    {
         pthread_mutex_lock(&lock);
-        printf("Thread Id: %d, Process ID %d, buffer: %d \n", *myid, getpid(), buffer++);
-        pthread_mutex_unlock(&lock);
-
-
-    return NULL;
+        if (buffer < 15)
+        {
+            printf("Thread Id: %d, Process ID %d, buffer: %d \n", *myid, getpid(), buffer++);
+            iterator++;
+            pthread_mutex_unlock(&lock);
+            sleep(0.01);
+        }
+        else
+        {
+            pthread_mutex_unlock(&lock);
+            return iterator;
+        }
+    }
 }
-  
+
 int main()
 {
-        pthread_t tid1, tid2, tid3;
+    pthread_t tid[3];
+    int counts[3], sum = 0;
 
-        pthread_mutex_init(&lock, NULL);
-        
-            pthread_create(&tid1, NULL, thread_function, (void *)&tid1);
-            pthread_create(&tid2, NULL, thread_function, (void *)&tid2);
-            pthread_create(&tid3, NULL, thread_function, (void *)&tid3);
-        for(int i = 0; i<5; i++){
-        pthread_join(tid1, NULL);
-        pthread_join(tid2, NULL);
-        pthread_join(tid3, NULL);
-        }
-        pthread_mutex_destroy(&lock);
+    pthread_mutex_init(&lock, NULL);
 
-        return 0;
+    for (int i = 0; i < 3; i++)
+    {
+        pthread_create(&tid[i], NULL, thread_function, (void *)&tid[i]);
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        pthread_join(tid[i], (int *)&counts[i]);
+        printf("TID %d worked on the buffer %d \033[0;32mtimes\033[0m \n", tid[i], counts[i]);
+        sum += counts[i];
+    }
+
+    printf("Total buffer accesses: %d \n", sum);
+
+    pthread_mutex_destroy(&lock);
+    return 0;
 }
