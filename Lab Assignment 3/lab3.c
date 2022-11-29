b9c1f23109ec9242c1a75c1d435cb0d166ad79f9
Fredrik <fredrik.thomas.lundstrom@gmail.com>
2022-11-29 19:24:36 +0100

    chicken dinner

--- Lab Assignment 3/lab3.c
+++ Lab Assignment 3/lab3.c
@@ -6,39 +6,75 @@
 
 int page_table[PAGE_ENTRIES];
 
-int get_page_number(int logical_address){
+int get_page_number(int logical_address)
+{
     return (0xFF00 & logical_address) >> 8;
 }
-int get_page_offset(int logical_address){
+
+int get_page_offset(int logical_address)
+{
     return (0x00FF & logical_address);
 }
-int get_physical_address(int logical_address){
+
+int get_physical_address(int logical_address)
+{
     int page_number = get_page_number(logical_address);
     int page_offset = get_page_offset(logical_address);
     int frame_number = page_table[page_number];
-    return (frame_number << 8) | page_offset;
+    // int frame_number = page_number;
+    // return (frame_number << 8) | page_offset;
+    return page_offset | frame_number;
+    // return (frame_number * 256) + page_offset;
 }
 
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
 
     FILE *fd;
-    fd=fopen(argv[1],"r");
+    fd = fopen("./data/addresses.txt", "r");
     char *value = NULL;
-    size_t len=0;
-    
-
-    getline(&value,&len,fd);
+    size_t len = 0;
 
     FILE *physical_memory = fopen(BIN_FILE, "rb");
 
+    memset(page_table, -1, sizeof(page_table));
 
-    int logical_address = atoi(value);
+    int read, frame, i;
+    while ((read = getline(&value, &len, fd)) != -1)
+    {
+        int logical_address = atoi(value);
 
-    printf("logical address %d page_number : %x page_offset : %x physical_address: %d \n", 
-                logical_address,
-                get_page_number(logical_address), 
-                get_page_offset(logical_address),
-                get_physical_address(logical_address));
+        int page_number = get_page_number(logical_address);
+        int page_offset = get_page_offset(logical_address);
+
+        int f = 0;
+        for (i = 0; i < PAGE_ENTRIES; i++)
+        {
+            if (page_table[i] == page_number)
+            {
+                frame = i;
+                //fault++;
+                break;
+            }   
+            if (page_table[i] == -1)
+            {
+                f = 1;
+
+                break;
+            }
+        }
+        if (f == 1)
+        {
+            page_table[i] = page_number;
+            frame = i;
+        }
+
+        printf("logical address %d page_number : %d page_offset : %d physical_address: %d \n",
+               logical_address,
+               get_page_number(logical_address),
+               get_page_offset(logical_address),
+               frame * 256 + page_offset);
+    }
 
     return 0;
 }
