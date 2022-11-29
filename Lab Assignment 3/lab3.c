6e799eb1fc8c153514ea9781c6d70686b5535fe8
Philiphagg <62904448+Digitusmedia@users.noreply.github.com>
2022-11-29 17:25:08 +0100

    added some stuff

--- Lab Assignment 3/lab3.c
+++ Lab Assignment 3/lab3.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #define PAGE_ENTRIES 256
+#define BIN_FILE "data/BACKING_STORE.bin"
 
+int page_table[PAGE_ENTRIES];
 
 int get_page_number(int logical_address){
     return (0xFF00 & logical_address) >> 8;
@@ -10,8 +12,12 @@ int get_page_number(int logical_address){
 int get_page_offset(int logical_address){
     return (0x00FF & logical_address);
 }
-
-int page_table[PAGE_ENTRIES];
+int get_physical_address(int logical_address){
+    int page_number = get_page_number(logical_address);
+    int page_offset = get_page_offset(logical_address);
+    int frame_number = page_table[page_number];
+    return (frame_number << 8) | page_offset;
+}
 
 int main(int argc, char* argv[]){
 
@@ -23,12 +29,16 @@ int main(int argc, char* argv[]){
 
     getline(&value,&len,fd);
 
+    FILE *physical_memory = fopen(BIN_FILE, "rb");
+
 
     int logical_address = atoi(value);
 
-    printf("logical address %d ", logical_address);
-    printf(" page_number : %x ", get_page_number(logical_address));
-    printf(" page_offset : %x \n", get_page_offset(logical_address));
+    printf("logical address %d page_number : %x page_offset : %x physical_address: %d \n", 
+                logical_address,
+                get_page_number(logical_address), 
+                get_page_offset(logical_address),
+                get_physical_address(logical_address));
 
     return 0;
 }
