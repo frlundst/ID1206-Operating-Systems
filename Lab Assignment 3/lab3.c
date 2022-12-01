d44583e16e43a61dbaf6f2651cab8686e3fe9068
Philiphagg <62904448+Digitusmedia@users.noreply.github.com>
2022-12-01 17:25:19 +0100

    lab 3 done

--- Lab Assignment 3/lab3.c
+++ Lab Assignment 3/lab3.c
@@ -3,8 +3,12 @@
 #include <string.h>
 #define PAGE_ENTRIES 256
 #define BIN_FILE "data/BACKING_STORE.bin"
+#define TLB_SIZE 16
 
 int page_table[PAGE_ENTRIES];
+int tlb[TLB_SIZE][2];
+int number_of_hits, number_of_misses, hit, qp;
+int number_of_occurencies = 0;
 
 int get_page_number(int logical_address)
 {
@@ -23,6 +27,8 @@ int get_physical_address(int logical_address, int frame_number)
     return frame_number * 256 + page_offset;
 }
 
+
+
 int main(int argc, char *argv[])
 {
 
@@ -35,34 +41,55 @@ int main(int argc, char *argv[])
     size_t len = 0;
 
     memset(page_table, -1, sizeof(page_table));
+    memset(tlb,-1,TLB_SIZE*2*sizeof(tlb[0][0]));
+
 
     int read, frame, i;
     while ((read = getline(&value, &len, addresses)) != -1)
     {
+        number_of_occurencies++;
         int logical_address = atoi(value);
-
+        
         int page_number = get_page_number(logical_address);
         int page_offset = get_page_offset(logical_address);
 
-        int f = 0;
-        for (i = 0; i < PAGE_ENTRIES; i++)
-        {
-            if (page_table[i] == page_number)
-            {
-                frame = i;
-                break;
-            }   
-            if (page_table[i] == -1)
-            {
-                f = 1;
+        hit = 0;
 
-                break;
+        for(i=0;i<TLB_SIZE;i++)
+		{
+			if(tlb[i][0]==page_number)
+			{
+				hit=1;
+				number_of_hits++;
+				frame=tlb[i][1];
+				break;
+			}
+		}
+        
+        if(hit == 0){
+            int f = 0;
+            for (i = 0; i < PAGE_ENTRIES; i++)
+            {
+                if (page_table[i] == page_number)
+                {
+                    frame = i;
+                    number_of_misses++;
+                    break;
+                }   
+                if (page_table[i] == -1)
+                {
+                    f = 1;
+
+                    break;
+                }
             }
-        }
-        if (f == 1)
-        {
-            page_table[i] = page_number;
-            frame = i;
+            if (f == 1)
+            {
+                page_table[i] = page_number;
+                frame = i;
+            }
+        tlb[qp%=15][0]=page_number;
+        tlb[qp++][1]=i;	
         }
 
         char value = 0;
@@ -75,6 +102,10 @@ int main(int argc, char *argv[])
                value
                );
     }
+    double hitrate = (double)number_of_hits/number_of_occurencies*100;
+    double missrate = (double)number_of_misses/number_of_occurencies*100;
+    printf("hitrate: %.2f%c\t\t missrate: %.2f%c \n", hitrate, '%', missrate, '%');
+    printf("number of hits: %d \t number of misses: %d  occ %d\n", number_of_hits, number_of_misses,number_of_occurencies);
 
     return 0;
 }
