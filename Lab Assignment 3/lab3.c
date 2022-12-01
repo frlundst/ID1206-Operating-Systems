679158cf75dde2e2c1f9daaa4d3b8fdab3611939
Philiphagg <62904448+Digitusmedia@users.noreply.github.com>
2022-12-01 14:12:12 +0100

    can read from storage

--- Lab Assignment 3/lab3.c
+++ Lab Assignment 3/lab3.c
@@ -16,31 +16,29 @@ int get_page_offset(int logical_address)
     return (0x00FF & logical_address);
 }
 
-int get_physical_address(int logical_address)
+int get_physical_address(int logical_address, int frame_number)
 {
     int page_number = get_page_number(logical_address);
     int page_offset = get_page_offset(logical_address);
-    int frame_number = page_table[page_number];
-    // int frame_number = page_number;
-    // return (frame_number << 8) | page_offset;
+    
     return page_offset | frame_number;
-    // return (frame_number * 256) + page_offset;
 }
 
 int main(int argc, char *argv[])
 {
 
-    FILE *fd;
-    fd = fopen("./data/addresses.txt", "r");
+    FILE *addresses, *physical_memory;
+
+    addresses = fopen("./data/addresses.txt", "r");
+    physical_memory = fopen(BIN_FILE, "rb");
+
     char *value = NULL;
     size_t len = 0;
-
-    FILE *physical_memory = fopen(BIN_FILE, "rb");
 
     memset(page_table, -1, sizeof(page_table));
 
     int read, frame, i;
-    while ((read = getline(&value, &len, fd)) != -1)
+    while ((read = getline(&value, &len, addresses)) != -1)
     {
         int logical_address = atoi(value);
 
@@ -53,7 +51,6 @@ int main(int argc, char *argv[])
             if (page_table[i] == page_number)
             {
                 frame = i;
-                //fault++;
                 break;
             }   
             if (page_table[i] == -1)
@@ -69,11 +66,16 @@ int main(int argc, char *argv[])
             frame = i;
         }
 
-        printf("logical address %d page_number : %d page_offset : %d physical_address: %d \n",
+        char value = 0;
+
+        fseek(physical_memory, logical_address, SEEK_SET);
+        fread(&value, sizeof(char), 1, physical_memory);
+        printf("logical address %d  physical_address: %d value: %d \n",
                logical_address,
-               get_page_number(logical_address),
-               get_page_offset(logical_address),
-               frame * 256 + page_offset);
+               get_physical_address(logical_address, frame),
+               //frame * 256 + page_offset,
+               value
+               );
     }
 
     return 0;
