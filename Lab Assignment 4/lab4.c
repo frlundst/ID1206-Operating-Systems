e0ccbf420001b5a7563d5442d728fe61669abd7f
Philiphagg <62904448+Digitusmedia@users.noreply.github.com>
2022-12-05 16:03:45 +0100

    some changes

--- Lab Assignment 4/lab4.c
+++ Lab Assignment 4/lab4.c
@@ -1,28 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define INTEGER_MAXVALUE 2147483647
 
 int giveRandom(int lower, int upper)
 {
         return (rand() % (upper - lower + 1)) + lower;
 }
 
-int FCFS (int initial_position, int disc[]){
+int FCFS (int initial_position, int *disc){
     int sum = 0;
     int temp = 0;
+    int arrlength = (sizeof(disc));
 
-    for(int i = 0; i < (int)( sizeof(disc) / sizeof(disc[0]); i++){
+    for(int i = 0; i < arrlength; i++){
         temp = initial_position - disc[i];
-        printf("%d \n", temp);
         if(temp < 0) {
             temp = temp*-1;
         }
         sum += temp;
         initial_position = disc[i];
-        printf("%d, %d, %d\n", temp, sum, initial_position);
     }
 
     return sum;
 }
+
+int SSTF (int initial_position, int *disc){
+    int sum = 0;
+
+    int temp = INTEGER_MAXVALUE;
+    int diff = 0;
+    int arrlength = (sizeof(disc));
+
+for(int i = 0; i < arrlength; i++){
+    for(int i = 0; i < arrlength; i++){
+        diff = abs(initial_position - disc[i]);
+        //printf("diff: %d\n", diff);
+        if(diff < temp && diff != 0){
+            temp = diff;
+        }
+    }
+    initial_position = disc[i];
+    disc[i] = 0;
+    printf("temp: %d\n", temp);
+    sum += temp;
+    temp = INTEGER_MAXVALUE;
+    //HÄR ÄR DET NÅGOT KNASIGT TROR JAG
+
+}
+    return sum;
+}
+
 
 int main(){
     int i = 0;
@@ -43,8 +70,8 @@ int main(){
         i++;
     }
 
-    int sum = FCFS(53,slide_numbers);
-    printf("%d\n", sum);
+    printf("FCFS: %d\n", FCFS(53,slide_numbers));
+    printf("SSTF: %d\n", SSTF(53,slide_numbers));
 
 
     return 0;
